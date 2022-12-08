fce824840485b2c56072d98884d124fda2946582
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-12-08 19:33:45 +0900

    [Add] forkとsyslog用の名前付きセマフォの初期化処理を追加
    forkはnum of philoの値を使って初期化
    syslogはバイナリセマフォっぽい感じで初期化

--- philo_bonus/includes/philosophers.h
+++ philo_bonus/includes/philosophers.h
@@ -2,6 +2,7 @@
 # define PHILOSOPHERS_H
 # include <errno.h>
 # include <pthread.h>
+# include <semaphore.h>
 # include <stdbool.h>
 # include <stdint.h>
 # include <stdio.h>
@@ -14,13 +15,20 @@
 # define ERR_ARGS "[Error] Invalid argument found in cmdline arguments"
 # define ERR_ARG_SIGN "[Error] Each argument must be positive integer number"
 
+# define SEM_FORK_NAME "forks"
+# define SEM_SYSLOG_NAME "syslog"
+# define SEM_OFLAG (O_CREAT | O_EXCL)
+# define SEM_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+
 typedef struct s_info
 {
-	int	num_of_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_times_must_eat;
+	int		num_of_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		num_of_times_must_eat;
+	sem_t	*forks;
+	sem_t	*sys_log;
 }	t_info;
 
 // cmdline_arguments.
@@ -30,6 +38,8 @@ int		is_each_cmdline_arguments_valid(t_info *info);
 
 // ft_atoi.c
 int		ft_atoi(const char *str);
+
+// utils.c
 size_t	ft_strlen(const char *s);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -25,6 +25,39 @@ t_info	*init_global_info(int argc, char **argv)
 	return (info);
 }
 
+int	init_named_semaphore(t_info *info)
+{
+	info->forks = sem_open(
+		SEM_FORK_NAME, SEM_OFLAG, SEM_MODE, info->num_of_philo);
+	if (info->forks == SEM_FAILED)
+	{
+		free(info);
+		return (1);
+	}
+	info->sys_log = sem_open(
+		SEM_SYSLOG_NAME, SEM_OFLAG, SEM_MODE, 1);
+	if (info->sys_log == SEM_FAILED)
+	{
+		sem_close(info->forks);
+		sem_unlink(SEM_FORK_NAME);
+		free(info);
+		return (1);
+	}
+	return (0);
+}
+
+void	unlink_all_semaphore(t_info *info)
+{
+	if (sem_close(info->forks) != 0)
+		perror("Unable to close semaphore");
+	if (sem_unlink(SEM_FORK_NAME) != 0)
+		perror("Unable to unlink semaphore");
+	if (sem_close(info->sys_log) != 0)
+		perror("Unable to close semaphore");
+	if (sem_unlink(SEM_SYSLOG_NAME) != 0)
+		perror("Unable to unlink semaphore");
+}
+
 int	main(int argc, char **argv)
 {
 	t_info	*info;
@@ -37,6 +70,10 @@ int	main(int argc, char **argv)
 	printf("time_to_eat: %d\n", info->time_to_eat);
 	printf("time_to_sleep: %d\n", info->time_to_sleep);
 	printf("min_eat_cnt: %d\n", info->num_of_times_must_eat);
+	if (init_named_semaphore(info) != 0)
+		exit(EXIT_FAILURE);
+
+	unlink_all_semaphore(info);
 	free(info);
 	exit(EXIT_SUCCESS);
 }
