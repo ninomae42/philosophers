5144f4885fde89241edee5e6827247390ba1280e
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-03-02 19:38:07 +0900

    [update] 必要なヘッダファイルをインクルード

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,16 @@
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+# include <errno.h>
+# include <limits.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
+#endif
