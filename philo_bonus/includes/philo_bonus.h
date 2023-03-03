6e290a157e48f2893553b71a4a8a1cc2a76faefd
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-03-03 09:00:04 +0900

    [update] 引数の受け取り、validate処理まで完了

--- philo_bonus/includes/philo_bonus.h
+++ philo_bonus/includes/philo_bonus.h
@@ -13,9 +13,34 @@
 # include <sys/stat.h>
 # include <unistd.h>
 
+# define ERR_ARG_FORMAT "[Error] Unable to convert some arguments to integer"
+# define ERR_ARG_NEGATIVE "[Error] Each arguments must be positive integer value"
+# define ERR_ARG_PHILO_NUM "[Error] Num of philos must be less than equal 200"
+
+# define MAX_PHILO_NUM 200
+
+// Command line arguments
+typedef struct s_args
+{
+	int	num_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_of_must_eat;
+}	t_args;
+
+// ft_atoi.c
+int	ft_atoi(const char *str);
+
 // ft_put_some.c
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_puterr(char *err_msg);
+
+// cmdline_args.c
+void	parse_cmdline_arguments(int argc, char **argv, t_args *args);
+
+// validate_args.c
+void	validate_arguments(int argc);
 
 #endif
