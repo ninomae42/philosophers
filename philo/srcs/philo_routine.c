4f06a5dbe87b37693390b40025ef9e98602cc2f4
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-03-18 15:35:08 +0900

    [fix] 1人のときのシミュレーションを変更

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,13 +6,13 @@
 /*   By: tashimiz <tashimiz@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 23:25:54 by tashimiz          #+#    #+#             */
-/*   Updated: 2023/03/18 14:53:17 by tashimiz         ###   ########.fr       */
+/*   Updated: 2023/03/18 15:33:16 by tashimiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static int	check_death_release_fork(t_philo *philo);
+static void	check_death_release_fork(t_philo *philo);
 
 void	*routine_philo(void *args)
 {
@@ -46,32 +46,28 @@ void	*routine_single_philo(void *args)
 	pthread_mutex_lock(philo->access_mutex);
 	philo->last_eat_time = get_current_time_in_msec();
 	pthread_mutex_unlock(philo->access_mutex);
-	while (1)
+	pthread_mutex_lock(philo->fork_right);
+	if (print_philo_log(philo, LOG_FORK) != 0)
 	{
-		pthread_mutex_lock(philo->fork_right);
-		if (print_philo_log(philo, LOG_FORK) != 0)
-		{
-			pthread_mutex_unlock(philo->fork_right);
-			break ;
-		}
-		ft_msleep(philo->time_to_die / 2);
-		ft_msleep(philo->time_to_die / 2);
-		if (check_death_release_fork(philo) != 0)
-			break ;
+		pthread_mutex_unlock(philo->fork_right);
+		return (args);
 	}
+	check_death_release_fork(philo);
 	return (args);
 }
 
-static int	check_death_release_fork(t_philo *philo)
+static void	check_death_release_fork(t_philo *philo)
 {
-	pthread_mutex_lock(philo->syslog_mutex);
-	if (philo->info->is_simulation_stop)
+	while (1)
 	{
-		pthread_mutex_unlock(philo->fork_right);
+		ft_msleep(philo->time_to_die);
+		pthread_mutex_lock(philo->syslog_mutex);
+		if (philo->info->is_simulation_stop)
+		{
+			pthread_mutex_unlock(philo->fork_right);
+			pthread_mutex_unlock(philo->syslog_mutex);
+			return ;
+		}
 		pthread_mutex_unlock(philo->syslog_mutex);
-		return (1);
 	}
-	pthread_mutex_unlock(philo->syslog_mutex);
-	pthread_mutex_unlock(philo->fork_right);
-	return (0);
 }
